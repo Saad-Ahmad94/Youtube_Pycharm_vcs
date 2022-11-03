76495420f649248b20bab17473af6eb01e1680fc
Saad Ahmad <saadahmed81994@gmail.com>
2022-11-03 01:19:29 +0100

    importing password

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # This is a sample Python script.
-
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+from secret import password
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -14,5 +14,6 @@ if __name__ == '__main__':
     print_hi('Saad')
     print('This is my World bro')
     print('Where do you live')
+    print(f'Passowrd is: {password}')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
